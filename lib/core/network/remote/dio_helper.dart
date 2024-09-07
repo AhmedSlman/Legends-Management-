2ccc05dc10ede8fd41fccce26374fe32cdf90301
ashry <ahmed.samy2050@yahoo.com>
2024-09-07 21:44:44 +0300

    adding dio and cache files

---
+++ lib/core/network/remote/dio_helper.dart
@@ -0,0 +1,61 @@
+// import 'package:dio/dio.dart';
+
+// import 'api_constant.dart';
+// import 'dio_interceptor.dart';
+
+// class DioHelper {
+//   static Dio dio = Dio();
+//   static init() {
+//     dio = Dio(
+//       BaseOptions(
+//         baseUrl: ApiConstance.baseUrl,
+//         receiveDataWhenStatusError: true,
+//       ),
+//     );
+//     dio.interceptors.add(
+//       MyDioInterceptor(),
+//     );
+//   }
+
+//   static Future<Response> postData(
+//       {required String endPoint,
+//       required Map<String, dynamic> data,
+//       String lang = 'en',
+//       String? token}) async {
+//     dio.options.headers = {
+//       'lang': lang,
+//       'Authorization': 'Bearer $token',
+//       'Content-Type': 'application/json',
+//       'Accept': 'application/json',
+//     };
+//     Response response = await dio.post(endPoint, data: data);
+//     return response;
+//   }
+
+//   static Future<Response> getData(
+//       {required String endPoint, String lang = 'en', String? token}) async {
+//     dio.options.headers = {
+//       'lang': lang,
+//       'Authorization': 'Bearer $token',
+//       'Content-Type': 'application/json',
+//       'Accept': 'application/json',
+//     };
+//     Response response = await dio.get(endPoint);
+//     return response;
+//   }
+
+//   static Future<Response> puttData(
+//       {required String endPoint,
+//       required Map<String, dynamic> data,
+//       String lang = 'en',
+//       String? token}) async {
+//     dio.options.headers = {
+//       'lang': lang,
+//       'Authorization': 'Bearer $token',
+//       'Content-Type': 'application/json',
+//       'Accept': 'application/json',
+//     };
+//     Response response = await dio.put(endPoint, data: data);
+//     return response;
+//   }
+// }
